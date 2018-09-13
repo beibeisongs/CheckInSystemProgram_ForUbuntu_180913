69ebb6f93b7b469a579a5127c83330d0aed9dbeb
MUJZY <zhengning009@sina.com>
2018-09-13 17:17:59 +0800

    Add files via upload

---
+++ Try_Function_1.py
@@ -0,0 +1,23 @@
+# encoding=utf-8
+# Date: 2018-09-13
+# Author: MJUZY
+
+
+import os
+import shutil
+
+
+if __name__ == "__main__":
+
+    """
+    >>>new_path = './class_name/name'
+    >>>if os.path.exists(new_path) == False:
+        >>>os.makedirs(new_path)
+        >>>print("The space has been created !")
+    >>>else:
+        >>>print("The space has been created before !")
+    """
+
+    """
+    >>>shutil.move('./123456_20171000718.jpg', 'D:/new_document')
+    """
