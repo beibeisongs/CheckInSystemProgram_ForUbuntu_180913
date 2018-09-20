2e7fd25fd97f1df7ecdda8422047fbb6fa73860a
MUJZY <zhengning009@sina.com>
2018-09-20 21:38:11 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,28 +1,17 @@
 # encoding=utf-8
-
 # Date: 2018-09-13
-
 # Author: MJUZY
 
 
-
-
-
 from ExtractMaxFace import extractProcess
-
 from FaceRecognzedProcess import faceRecognzedProcess
-
 import dlib
-
 from flask import Flask, url_for
-
 from flask import request
-
 import shutil
-
 from werkzeug.utils import secure_filename
-
 import os
+
 
 app = Flask(__name__)
 
@@ -31,27 +20,19 @@ os.system("python /var/www/demoapp/Try_Function_2.py")
 
 def prepare_detector(predictor_path, face_rec_model_path):
     file = open("/var/www/demoapp/show_hello_running.txt", 'a')
-
     file.write("Prepare_detector !\n")
 
     # 加载正脸检测器
-
     detector = dlib.get_frontal_face_detector()
-
     file.write("detector prepared !\n")
 
     # 加载人脸关键点检测器
-
     sp = dlib.shape_predictor(predictor_path)
-
     file.write("sp prepared !\n")
 
     # 加载人脸识别模型
-
     facerec = dlib.face_recognition_model_v1(face_rec_model_path)
-
     file.write("facerec prepared !\n")
-
     file.close()
 
     return detector, sp, facerec
@@ -59,42 +40,26 @@ def prepare_detector(predictor_path, face_rec_model_path):
 
 def prepare_path_etc():
     # 人脸关键点检测器
-
     predictor_path = "/var/www/demoapp/shape_predictor_68_face_landmarks.dat"
-
     # 人脸识别模型：
-
     face_rec_model_path = "/var/www/demoapp/dlib_face_recognition_resnet_model_v1.dat"
-
     return predictor_path, face_rec_model_path
 
 
 predictor_path, face_rec_model_path = prepare_path_etc()
-
 detector, sp, facerec = prepare_detector(predictor_path, face_rec_model_path)
 
 """Attention:
-
     This is just the sample of the usage of the cache
 
-
-
-from werkzeug.contrib.cache import SimpleCache
-
-
+>>>from werkzeug.contrib.cache import SimpleCache
 
 >>>cache = SimpleCache()
-
 >>>cache.set("detector", detector)
-
 >>>cache.set("sp", sp)
 
 >>>cache.set("facerec", facerec)
-
-
-
 >>>cache.get("detector")
-
 >>>...
 
 """
@@ -105,22 +70,15 @@ from werkzeug.contrib.cache import SimpleCache
 @app.route('/hello')
 def api_hello():
     if 'name' in request.args:
-
         return 'Hello ' + request.args['name']
-
     else:
-
         return 'Hello John Doe'
 
 
 # Method Two/First: curl http://127.0.0.1:5000/articles/abc
-
 # Output: You are reading abc
-
 # Method Two/Second: curl http://127.0.0.1:5000/articles
-
 # Output: List of /articles
-
 @app.route('/')
 def api_root():
     return 'Welcome'
@@ -137,55 +95,30 @@ def api_article(articleid):
 
 
 # Method Three:
-
 # C:\Users\zheng>curl -X Get http://127.0.0.1:5000/echo
-
 # ECHO: GET
-
 @app.route('/echo', methods=['GET', 'POST', 'PATCH', 'PUT', 'DELETE'])
 def api_echo():
     if request.method == 'GET':
-
         return "ECHO: GET\n"
-
-
-
     elif request.method == 'POST':
-
         return "ECHO: POST\n"
-
-
-
     elif request.method == 'PATCH':
-
         return "ECHO: PACTH\n"
-
-
-
     elif request.method == 'PUT':
-
         return "ECHO: PUT\n"
-
-
-
     elif request.method == 'DELETE':
-
         return "ECHO: DELETE"
 
 
 # Method Four: see Client\__init__.py
-
 # app.config['UPLOAD_FOLDER'] = 'D:\\PyFlaskLearningProjects\\20180613_Test1\\static\\uploads'
-
 # The route below is used for Ubuntu, upside for Windows
-
 app.config['UPLOAD_FOLDER'] = '/var/www/demoapp/student_photo'
-
 app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg', 'gif'])
 
 
 # For a given file, return whether it's an allowed type or not
-
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
@@ -193,7 +126,6 @@ def allowed_file(filename):
 @app.route('/sign_up_photo_upload', methods=['POST'])
 def upload():
     name = request.form.get('name', 'little apple')
-
     class_name = request.form.get('class_name', 'little apple')
 
     new_path = "/var/www/demoapp/Accounts/" + class_name + "/" + name
@@ -202,7 +134,6 @@ def upload():
         os.makedirs(new_path)
 
         # Create the document storing CSV File, which used to recored customs' Travel coordinations
-
         new_csv_path = new_path + "/" + "coordinations"
 
         os.makedirs(new_csv_path)
@@ -229,19 +160,12 @@ def upload():
 
         """Description: the orders following are used to check if the scripts have been correctly executed
 
-
-
         >>>log_file = open('/var/www/demoapp/log.txt', mode='a')
-
         >>>log_file.write("---shutil.move Ok !---")
 
         """
 
         IP = request.remote_addr
-
-        """Face Extraction Part : 
-
-        """
 
         dir = '/var/www/demoapp/Accounts'
 
@@ -250,7 +174,6 @@ def upload():
         return 'hello, ' + name + ' class_name: ' + class_name + 'IP : ' + IP + ' success'
 
     else:
-
         return 'hello, ' + request.form.get('name', 'little apple') + ' failed'
 
 
@@ -264,6 +187,7 @@ def checkin_upload1():
     :return:
 
     """
+    side_rate = 0
 
     result = "This is the Original Value of the result !"
 
@@ -288,7 +212,7 @@ def checkin_upload1():
 
         class_name = request.form.get('class_name', 'little apple')
 
-        result = faceRecognzedProcess(detector_forcheckin, sp_forcheckin, facerec_forcheckin,
+        result, side_rate = faceRecognzedProcess(detector_forcheckin, sp_forcheckin, facerec_forcheckin,
                                       dir_path, class_name,name,
                                       dirpath_forcheckin, upload_file.filename)
 
@@ -297,6 +221,7 @@ def checkin_upload1():
     if result == "Good !":
         mark_path = dir_path + '/' + class_name + '/' + name + '/' + "mark1.txt"
         file_mark = open(mark_path, 'w')
+        file_mark.write(str(side_rate))
         file_mark.close()
 
     return result
@@ -312,6 +237,7 @@ def checkin_upload2():
     :return:
 
     """
+    side_rate = 0
 
     result = "This is the Original Value of the result !"
 
@@ -336,7 +262,7 @@ def checkin_upload2():
 
         class_name = request.form.get('class_name', 'little apple')
 
-        result = faceRecognzedProcess(detector_forcheckin, sp_forcheckin, facerec_forcheckin,
+        result, side_rate = faceRecognzedProcess(detector_forcheckin, sp_forcheckin, facerec_forcheckin,
                                       dir_path, class_name,name,
                                       dirpath_forcheckin, upload_file.filename)
 
@@ -345,6 +271,14 @@ def checkin_upload2():
     if result == "Good !":
         mark_path = dir_path + '/' + class_name + '/' + name + '/' + "mark1.txt"
         if os.path.exists(mark_path) == True:
+            file_mark = open(mark_path, 'r')
+
+            side_rate_1 = file_mark.read()
+            if abs(float(side_rate_1) - float(side_rate)) == 0:
+                result = "Same Side Rate !!!"
+
+            file_mark.close()
+
             os.remove(mark_path)
 
             return result
@@ -385,8 +319,7 @@ def api_create_space(class_name, name):
 
 
 if __name__ == "__main__":
+
     # app.run(host='127.0.0.1', port=5001)
-
     # Below for Ubuntu, upside for Windows
-
     app.run(host='0.0.0.0', port=5001)
