5c174f42dc889c477ad10fb900101e27a3734294
MUJZY <zhengning009@sina.com>
2018-10-03 14:27:17 +0800

    Update Client_1.py

--- Client_1.py
+++ Client_1.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     # r = requests.post("http://0.0.0.0:5001/upload", data=user_info, files=files)
 
     files = {'image01': open('./kkk.jpg', 'rb')}
-    user_info = {'class_name': '123456', 'name': '20171000716'}
+    user_info = {'class_name': '123456', 'name': '20171000714'}
 
     # r = requests.post("http://120.79.132.142:5001/upload", data=user_info, files=files)
     # The Codes upside used for Windows to upload file to Ubuntu, but not passed !
@@ -26,7 +26,9 @@ if __name__ == "__main__":
     # The Codes below used for Windows to upload file to Ubuntu, and proved Correct !
 
     """r = requests.post("http://120.79.132.142/sign_up_photo_upload", data=user_info, files=files)"""
-    r = requests.post("http://120.79.132.142/checkin_photo_upload2", data=user_info, files=files)
+    """r = requests.post("http://120.79.132.142/checkin_photo_upload1", data=user_info, files=files)"""
+    r = requests.post("http://120.79.132.142/post_function_test1", data=user_info)
+    """r = requests.post("http://www.baidu.com")"""
     """r = requests.post("http://120.79.132.142/checkin_photo_upload2", data=user_info, files=files)"""
 
     print(r.text)
